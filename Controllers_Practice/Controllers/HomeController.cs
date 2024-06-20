360fcf6da12ff004b1559e8eb509390359f9fbb9
NehanPathan <pathannehan26@gmail.com>
2024-06-20 06:18:36 +0530

    Add more action methods and route attribute

--- Controllers_Practice/Controllers/HomeController.cs
+++ Controllers_Practice/Controllers/HomeController.cs
@@ -3,12 +3,27 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Controllers_Practice.Controllers
 {
+    [Controller]
     public class HomeController
     {
         [Route("sayhello")]
-        public string method1()
+        [Route("/")]
+        public string Index()
         {
-            return "Hello from method1";
+            return "Hello from Index";
         }
+
+        [Route("about")]
+        public string About()
+        {
+            return "Hello from About";
+        }
+
+        [Route("contact-us/{mobile:regex(^\\d{{10}}$)}")]
+        public string Contact()
+        {
+            return "Hello from Contact";
+        }
+
     }
 }
