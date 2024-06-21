d326f0d2bb06c6cd2156ccabed1bc9407a45dd57
NehanPathan <pathannehan26@gmail.com>
2024-06-21 06:28:26 +0530

    Learn File Result type

--- Controllers_Practice/Controllers/HomeController.cs
+++ Controllers_Practice/Controllers/HomeController.cs
@@ -19,18 +19,37 @@ namespace Controllers_Practice.Controllers
         [Route("person")]
         public JsonResult Person()
         {
-            Person person = new Person(){
-                Id = Guid.NewGuid(), FirstName = "Nehaan", LastName = "Khan", Age = 19
+            Person person = new Person()
+            {
+                Id = Guid.NewGuid(),
+                FirstName = "Nehaan",
+                LastName = "Khan",
+                Age = 19
             };
             // return new JsonResult(person);
             return Json(person);
             // return "{ \"key\": \"value\"}";
         }
 
-        [Route("contact-us/{mobile:regex(^\\d{{10}}$)}")]
-        public string Contact()
+        [Route("file-download")]
+        public VirtualFileResult FileDownload()
         {
-            return "Hello from Contact";
+            // return new VirtualFileResult("/Certificate.pdf", "application/pdf");
+            return File("/Certificate.pdf", "application/pdf");
+        }
+        [Route("file-download2")]
+        public PhysicalFileResult FileDownload2()
+        {
+            // return new PhysicalFileResult(@"C:\Users\patha\OneDrive\Desktop\ASP.NET Core Practice\Certificate.pdf", "application/pdf");
+            return PhysicalFile(@"C:\Users\patha\OneDrive\Desktop\ASP.NET Core Practice\Certificate.pdf", "application/pdf");
+        }
+
+        [Route("file-download3")]
+        public FileContentResult FileDownload3()
+        {
+            byte[] bytes = System.IO.File.ReadAllBytes(@"C:\Users\patha\OneDrive\Desktop\ASP.NET Core Practice\Certificate.pdf");
+            // return new FileContentResult(bytes, "application/pdf");
+            return File(bytes, "application/pdf");
         }
 
     }
