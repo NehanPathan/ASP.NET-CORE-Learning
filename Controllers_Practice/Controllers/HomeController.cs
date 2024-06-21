dd755d915249af3d8a4a739ef7fc1f6286c412f0
NehanPathan <pathannehan26@gmail.com>
2024-06-21 22:23:32 +0530

    Learn IActionResult And StatusCodeResult  type

--- Controllers_Practice/Controllers/HomeController.cs
+++ Controllers_Practice/Controllers/HomeController.cs
@@ -51,6 +51,48 @@ namespace Controllers_Practice.Controllers
             // return new FileContentResult(bytes, "application/pdf");
             return File(bytes, "application/pdf");
         }
+        [Route("book")]
+        public IActionResult IActionResultUse()
+        {
+            //Book id should be applied
+            if (!Request.Query.ContainsKey("bookid"))
+            {
+                // return new BadRequestResult();
+                return BadRequest("Book id is not supplied");
+            }
+
+            //Book id can't be empty
+            if (string.IsNullOrEmpty(Convert.ToString(Request.Query["bookid"])))
+            {
+
+                return BadRequest("Book id can't be null or empty");
+            }
+
+            //Book id should be between 1 to 1000
+            int bookId = Convert.ToInt16(ControllerContext.HttpContext.Request.Query["bookid"]);
+            if (bookId <= 0)
+            {
+
+                return BadRequest("Book id can't be less than or equal to zero");
+            }
+            if (bookId > 1000)
+            {
+
+                return NotFound("Book id can't be greater than 1000");
+            }
+
+            //isloggedin should be true
+            if (Convert.ToBoolean(Request.Query["isloggedin"]) == false)
+            {
+
+                // return Unauthorized("User must be authenticated");
+                // return StatusCode(401);
+                return StatusCode(401, "User must be authenticated");
+
+            }
+
+            return File("/Certificate.pdf", "application/pdf");
+        }
 
     }
 }
--- Controllers_Practice/obj/Debug/net8.0/Controllers_Practice.AssemblyInfo.cs
+++ Controllers_Practice/obj/Debug/net8.0/Controllers_Practice.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Controllers_Practice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c731f99f23b68e168e3442af75b470f46f808b8e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d326f0d2bb06c6cd2156ccabed1bc9407a45dd57")]
 [assembly: System.Reflection.AssemblyProductAttribute("Controllers_Practice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Controllers_Practice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
