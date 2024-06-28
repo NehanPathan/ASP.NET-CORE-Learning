5283659f24b030db6b510189bf2c9a4bc1ee7e0f
NehanPathan <pathannehan26@gmail.com>
2024-06-28 06:39:17 +0530

    Learn All  Model Validation

--- Model_Validation_Example/obj/Debug/net8.0/Model_Validation_Example.AssemblyInfo.cs
+++ Model_Validation_Example/obj/Debug/net8.0/Model_Validation_Example.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Model_Validation_Example")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+691f6b8120ab73d7972680601a0793463e85c6b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+393e8e2429c59cdb3b9495546fc05c308e18d200")]
 [assembly: System.Reflection.AssemblyProductAttribute("Model_Validation_Example")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Model_Validation_Example")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
