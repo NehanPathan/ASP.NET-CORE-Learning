9ffd9f91e6e6226a016d799c01bd361e2612fac5
NehanPathan <pathannehan26@gmail.com>
2024-07-09 06:41:31 +0530

    For Xml Data Use FromBody By Adding AddXmlSerializerFormatters

--- Model_Validation_Example/Program.cs
+++ Model_Validation_Example/Program.cs
@@ -1,5 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
+builder.Services.AddControllers().AddXmlSerializerFormatters();
 var app = builder.Build();
 
 app.UseStaticFiles();
