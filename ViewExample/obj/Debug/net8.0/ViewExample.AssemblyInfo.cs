d082d9fe995a8435c8dd3cdde2879f0c849f9e50
NehanPathan <pathannehan26@gmail.com>
2024-07-29 06:34:06 +0530

    Learn to use foreach and for loop in Razor view

--- ViewExample/obj/Debug/net8.0/ViewExample.AssemblyInfo.cs
+++ ViewExample/obj/Debug/net8.0/ViewExample.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ViewExample")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70dcbd192d93c2cd4f0cab1fc1b7eb9545b1caa5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+104f44c1f36a8d307f4f851f08718d0f9658eb00")]
 [assembly: System.Reflection.AssemblyProductAttribute("ViewExample")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ViewExample")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
