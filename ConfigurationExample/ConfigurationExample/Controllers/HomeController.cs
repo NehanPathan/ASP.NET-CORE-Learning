32f9ec5821d87aee819faa9142cdc5a7ac07672c
NehanPathan <pathannehan26@gmail.com>
2024-08-26 12:56:13 +0530

    Understand the Environment specific config

--- ConfigurationExample/ConfigurationExample/Controllers/HomeController.cs
+++ ConfigurationExample/ConfigurationExample/Controllers/HomeController.cs
@@ -4,40 +4,26 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 
 namespace ConfigurationExample.Controllers
 {
     public class HomeController : Controller
     {
         //private field
-        private readonly IConfiguration _configuration;
-        public HomeController(IConfiguration configuration){
-            _configuration = configuration;
+        private readonly WeatherApiOptions _options;
+        public HomeController(IOptions<WeatherApiOptions> weatherApiOptions){
+            _options = weatherApiOptions.Value;
         }
 
 
         [Route("/")]
         public IActionResult Index()
         {
-            // ViewBag.ClientId = _configuration["weatherapi:ClientId"];
-            // ViewBag.ClientSecret = _configuration.GetValue("weatherapi:ClientSecret","the default ClientSecret value");
+            
 
-            // IConfiguration weatherapi = _configuration.GetSection("weatherapi");
-
-            //  ViewBag.ClientId = weatherapi["ClientId"];
-            // ViewBag.ClientSecret = weatherapi.GetValue("ClientSecret","the default ClientSecret value");
-
-
-            //Get : Loads configuration values into new Options object
-            // WeatherApiOptions options = _configuration.GetSection("weatherapi").Get<WeatherApiOptions>();
-
-            //Bind : Loads configuration values into existing Options object
-            WeatherApiOptions options = new WeatherApiOptions();
-            _configuration.GetSection("weatherapi").Bind(options);
-
-
-             ViewBag.ClientId = options.ClientId;
-            ViewBag.ClientSecret = options.ClientSecret;
+             ViewBag.ClientId = _options.ClientId;
+            ViewBag.ClientSecret = _options.ClientSecret;
 
 
             return View();
--- ConfigurationExample/ConfigurationExample/Program.cs
+++ ConfigurationExample/ConfigurationExample/Program.cs
@@ -1,5 +1,12 @@
+using ConfigurationExample;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
+
+
+//supply an object of WeatherApiOptions (with 'weatherapi' section) as a service
+builder.Services.Configure<WeatherApiOptions>(builder.Configuration.GetSection("weatherapi"));
+
 var app = builder.Build();
 
 app.UseRouting();
