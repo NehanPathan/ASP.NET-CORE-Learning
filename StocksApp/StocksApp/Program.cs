e733e151dda8354a061662f99b5dea98bffd8711
NehanPathan <pathannehan26@gmail.com>
2024-08-29 14:08:34 +0530

    Start HttpClient

---
+++ StocksApp/StocksApp/Program.cs
@@ -0,0 +1,9 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+builder.Services.AddHttpClient();
+var app = builder.Build(); 
+
+app.UseStaticFiles();
+app.UseRouting();
+app.MapControllers();
+app.Run();
