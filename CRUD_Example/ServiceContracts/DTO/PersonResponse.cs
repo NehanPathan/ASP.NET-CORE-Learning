02860bfc644a9299f92b75d30699835823fd3be4
NehanPathan <pathannehan26@gmail.com>
2024-09-05 10:08:26 +0530

    Add PersonUpdateRequest DTO

--- CRUD_Example/ServiceContracts/DTO/PersonResponse.cs
+++ CRUD_Example/ServiceContracts/DTO/PersonResponse.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Entities;
+using ServiceContracts.Enums;
 
 namespace ServiceContracts.DTO
 {
@@ -43,7 +44,13 @@ namespace ServiceContracts.DTO
         {
             return $"Person ID: {PersonID}, Person Name: {PersonName}, Email: {Email}, Date of Birth: {DateOfBirth?.ToString("dd MMM yyyy")}, Gender: {Gender}, Country ID: {CountryID}, Country: {Country}, Address: {Address}, Receive News Letters: {ReceiveNewsLetters}";
         }
+
+        public PersonUpdateRequest ToPersonUpdateRequest()
+        {
+            return new PersonUpdateRequest() { PersonID = PersonID, PersonName = PersonName, Email = Email, DateOfBirth = DateOfBirth, Gender = (GenderOptions)Enum.Parse(typeof(GenderOptions), Gender, true), Address = Address, CountryID = CountryID, ReceiveNewsLetters = ReceiveNewsLetters };
+        }
     }
+
 
     public static class PersonExtensions
     {
