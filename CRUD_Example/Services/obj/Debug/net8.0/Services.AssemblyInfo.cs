02d110a9289690eeccedbb9eac9e5679f53089af
NehanPathan <pathannehan26@gmail.com>
2024-09-05 09:27:23 +0530

    implement  GetFiltered Person

--- CRUD_Example/Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
+++ CRUD_Example/Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea7b74c78debca60612380b19f00eef67f0a433c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0da4fa8d6f1ab38f5a6fd5cffb8da75e2b82c128")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
