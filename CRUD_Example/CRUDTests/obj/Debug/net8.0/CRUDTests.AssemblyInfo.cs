0a0e213eabf75d3b6be42143ed9b88a54482e58c
NehanPathan <pathannehan26@gmail.com>
2024-09-05 09:52:30 +0530

    Add Unit test for GetSortedPerson

--- CRUD_Example/CRUDTests/obj/Debug/net8.0/CRUDTests.AssemblyInfo.cs
+++ CRUD_Example/CRUDTests/obj/Debug/net8.0/CRUDTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRUDTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7206ab500abc38b195e9845d111753a7b1f473a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02d110a9289690eeccedbb9eac9e5679f53089af")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRUDTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRUDTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
