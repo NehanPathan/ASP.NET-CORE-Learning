9e40bb3b9f28e285acdd0ad997dc68b31414bb00
NehanPathan <pathannehan26@gmail.com>
2024-11-04 22:51:44 +0530

    Use AutoFixture for sample generation in PersonServiceTest

--- DOTNETPRACTICE/WeatherApp/CRUDExample/obj/Debug/net6.0/CRUDExample.AssemblyInfo.cs
+++ DOTNETPRACTICE/WeatherApp/CRUDExample/obj/Debug/net6.0/CRUDExample.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRUDExample")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b3d7871f07e87423df2449f4d78417cb15a58ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ecef55e3281b01dd5ed2fb78760361c872bc24a")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRUDExample")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRUDExample")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- DOTNETPRACTICE/WeatherApp/CRUDTests/obj/Release/net6.0/CRUDTests.AssemblyInfo.cs
+++ DOTNETPRACTICE/WeatherApp/CRUDTests/obj/Release/net6.0/CRUDTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRUDTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b3d7871f07e87423df2449f4d78417cb15a58ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ecef55e3281b01dd5ed2fb78760361c872bc24a")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRUDTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRUDTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
