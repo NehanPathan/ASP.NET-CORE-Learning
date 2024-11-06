46b8b584f44a50a5b5c5347ab6c2dd66d5e6df3a
NehanPathan <pathannehan26@gmail.com>
2024-11-06 21:54:02 +0530

    Start Implementing Repository Pattern And Done The Half Part

--- DOTNETPRACTICE/WeatherApp/CRUDExample/obj/Debug/net6.0/CRUDExample.AssemblyInfo.cs
+++ DOTNETPRACTICE/WeatherApp/CRUDExample/obj/Debug/net6.0/CRUDExample.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRUDExample")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ecef55e3281b01dd5ed2fb78760361c872bc24a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49beccd17b09e6f0e67fdc4a0ceff2d208438794")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRUDExample")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRUDExample")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- DOTNETPRACTICE/WeatherApp/CRUDTests/obj/Debug/net6.0/CRUDTests.AssemblyInfo.cs
+++ DOTNETPRACTICE/WeatherApp/CRUDTests/obj/Debug/net6.0/CRUDTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRUDTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e40bb3b9f28e285acdd0ad997dc68b31414bb00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49beccd17b09e6f0e67fdc4a0ceff2d208438794")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRUDTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRUDTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
