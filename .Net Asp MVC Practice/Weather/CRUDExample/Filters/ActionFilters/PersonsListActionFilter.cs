16e057435234559440bdd47f81958a5c30284135
NehanPathan <pathannehan26@gmail.com>
2025-01-01 21:59:00 +0530

    UI Enhancement For Other Pages

--- .Net Asp MVC Practice/Weather/CRUDExample/Filters/ActionFilters/PersonsListActionFilter.cs
+++ .Net Asp MVC Practice/Weather/CRUDExample/Filters/ActionFilters/PersonsListActionFilter.cs
@@ -1,51 +1,60 @@
 ﻿using CRUDExample.Controllers;
 using Microsoft.AspNetCore.Mvc.Filters;
 using ServiceContracts.DTO;
+using ServiceContracts.Enums;
 
 namespace CRUDExample.Filters.ActionFilters
 {
- public class PersonsListActionFilter : IActionFilter
- {
-  private readonly ILogger<PersonsListActionFilter> _logger;
-
-  public PersonsListActionFilter(ILogger<PersonsListActionFilter> logger)
-  {
-   _logger = logger;
-  }
-
-  public void OnActionExecuted(ActionExecutedContext context)
-  {
-   //To do: add after logic here
-   _logger.LogInformation("{FilterName}.{MethodName} method", nameof(PersonsListActionFilter), nameof(OnActionExecuted));
-
-   PersonsController personsController = (PersonsController)context.Controller;
-
-   IDictionary<string, object?>? parameters = (IDictionary<string, object?>?) context.HttpContext.Items["arguments"];
-
-   if (parameters != null)
-   {
-    if (parameters.ContainsKey("searchBy"))
+    public class PersonsListActionFilter : IActionFilter
     {
-     personsController.ViewData["CurrentSearchBy"] = Convert.ToString(parameters["searchBy"]);
-    }
+        private readonly ILogger<PersonsListActionFilter> _logger;
 
-    if (parameters.ContainsKey("searchString"))
-    {
-     personsController.ViewData["CurrentSearchString"] = Convert.ToString(parameters["searchString"]);
-    }
+        public PersonsListActionFilter(ILogger<PersonsListActionFilter> logger)
+        {
+            _logger = logger;
+        }
 
-    if (parameters.ContainsKey("sortBy"))
-    {
-     personsController.ViewData["CurrentSortBy"] = Convert.ToString(parameters["sortBy"]);
-    }
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            //To do: add after logic here
+            _logger.LogInformation("{FilterName}.{MethodName} method", nameof(PersonsListActionFilter), nameof(OnActionExecuted));
 
-    if (parameters.ContainsKey("sortOrder"))
-    {
-     personsController.ViewData["CurrentSortOrder"] = Convert.ToString(parameters["sortOrder"]);
-    }
-   }
+            PersonsController personsController = (PersonsController)context.Controller;
 
-   personsController.ViewBag.SearchFields = new Dictionary<string, string>()
+            IDictionary<string, object?>? parameters = (IDictionary<string, object?>?)context.HttpContext.Items["arguments"];
+
+            if (parameters != null)
+            {
+                if (parameters.ContainsKey("searchBy"))
+                {
+                    personsController.ViewData["CurrentSearchBy"] = Convert.ToString(parameters["searchBy"]);
+                }
+
+                if (parameters.ContainsKey("searchString"))
+                {
+                    personsController.ViewData["CurrentSearchString"] = Convert.ToString(parameters["searchString"]);
+                }
+
+                if (parameters.ContainsKey("sortBy"))
+                {
+                    personsController.ViewData["CurrentSortBy"] = Convert.ToString(parameters["sortBy"]);
+                }
+                else
+                {
+                    personsController.ViewData["CurrentSortBy"] = nameof(PersonResponse.PersonName);
+                }
+
+                if (parameters.ContainsKey("sortOrder"))
+                {
+                    personsController.ViewData["CurrentSortOrder"] = Convert.ToString(parameters["sortOrder"]);
+                }
+                else
+                {
+                    personsController.ViewData["CurrentSortOrder"] = nameof(SortOrderOptions.ASC);
+                }
+            }
+
+            personsController.ViewBag.SearchFields = new Dictionary<string, string>()
       {
         { nameof(PersonResponse.PersonName), "Person Name" },
         { nameof(PersonResponse.Email), "Email" },
@@ -55,24 +64,24 @@ namespace CRUDExample.Filters.ActionFilters
         { nameof(PersonResponse.Address), "Address" }
       };
 
-  }
+        }
 
 
-  public void OnActionExecuting(ActionExecutingContext context)
-  {
-   context.HttpContext.Items["arguments"] = context.ActionArguments;
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            context.HttpContext.Items["arguments"] = context.ActionArguments;
 
-   //To do: add before logic here
-   _logger.LogInformation("{FilterName}.{MethodName} method", nameof(PersonsListActionFilter), nameof(OnActionExecuting));
+            //To do: add before logic here
+            _logger.LogInformation("{FilterName}.{MethodName} method", nameof(PersonsListActionFilter), nameof(OnActionExecuting));
 
-   if (context.ActionArguments.ContainsKey("searchBy"))
-   {
-    string? searchBy = Convert.ToString(context.ActionArguments["searchBy"]);
+            if (context.ActionArguments.ContainsKey("searchBy"))
+            {
+                string? searchBy = Convert.ToString(context.ActionArguments["searchBy"]);
 
-    //validate the searchBy parameter value
-    if (!string.IsNullOrEmpty(searchBy))
-    {
-     var searchByOptions = new List<string>() { 
+                //validate the searchBy parameter value
+                if (!string.IsNullOrEmpty(searchBy))
+                {
+                    var searchByOptions = new List<string>() {
       nameof(PersonResponse.PersonName),
       nameof(PersonResponse.Email),
       nameof(PersonResponse.DateOfBirth),
@@ -81,16 +90,16 @@ namespace CRUDExample.Filters.ActionFilters
       nameof(PersonResponse.Address)
      };
 
-     //reset the searchBy paramer value
-     if (searchByOptions.Any(temp => temp == searchBy) == false)
-     {
-      _logger.LogInformation("searchBy actual value {searchBy}", searchBy);
-      context.ActionArguments["searchBy"] = nameof(PersonResponse.PersonName);
-      _logger.LogInformation("searchBy updated value {searchBy}", context.ActionArguments["searchBy"]);
-     }
-    }
-   }
+                    //reset the searchBy paramer value
+                    if (searchByOptions.Any(temp => temp == searchBy) == false)
+                    {
+                        _logger.LogInformation("searchBy actual value {searchBy}", searchBy);
+                        context.ActionArguments["searchBy"] = nameof(PersonResponse.PersonName);
+                        _logger.LogInformation("searchBy updated value {searchBy}", context.ActionArguments["searchBy"]);
+                    }
+                }
+            }
 
-  }
- }
+        }
+    }
 }
--- .Net Asp MVC Practice/Weather/RepositoryContracts/obj/Debug/net6.0/RepositoryContracts.AssemblyInfo.cs
+++ .Net Asp MVC Practice/Weather/RepositoryContracts/obj/Debug/net6.0/RepositoryContracts.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RepositoryContracts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e449b1791071597f0703d3800a1f3cac502f96d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bb723b55d2e7c77065617f0015f6f253bb3dfbe")]
 [assembly: System.Reflection.AssemblyProductAttribute("RepositoryContracts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RepositoryContracts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
