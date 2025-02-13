ff876cd389ea7ea1f1dc4c6d4ee9a0941498e18a
NehanPathan <pathannehan26@gmail.com>
2025-02-13 21:07:42 +0530

    Add Http Request

--- All In One/MinimalAPISolution/MinimalAPI/Program.cs
+++ All In One/MinimalAPISolution/MinimalAPI/Program.cs
@@ -1,13 +1,28 @@
+using MinimalAPI.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", async (HttpContext context) =>
+List<Product> products = new()
 {
-    await context.Response.WriteAsync("GET - Hello World!");
+    new Product { Id = 1, ProductName = "Smart Phone" },
+    new Product { Id = 2, ProductName = "Laptop" },
+    new Product { Id = 3, ProductName = "Tablet" }
+}; 
+
+//GET /products
+app.MapGet("/products", async (HttpContext context) =>
+{
+    var content = string.Join("\n", products.Select(p => p.ToString()));
+    //1, xxxx
+    //2, yyyy
+
+    await context.Response.WriteAsync(content);
 });
-app.MapPost("/", async (HttpContext context) =>
+app.MapPost("/products", async (HttpContext context, Product product) =>
 {
-    await context.Response.WriteAsync("POST - Hello World!");
+    products.Add(product);
+    await context.Response.WriteAsync("Product Added");
 });
 
 app.Run();
