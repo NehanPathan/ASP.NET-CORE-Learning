7e48549bfd725dc59a18d213ade4e246519baa82
NehanPathan <pathannehan26@gmail.com>
2025-03-08 22:03:47 +0530

    Update the code

---
+++ All In One/Chat/API/API/Program.cs
@@ -0,0 +1,28 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(x => x.UseSqlite("Data Source=chat.db"));
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+   
+}
+
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+
+
+app.Run();
--- All In One/Chat/API/Data/AppDbContext.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-namespace API.Data;
-
-public class AppDbContext: IdentityDb
-{
-
-}
